256d449aa9f93760aef436093683d13a319c2cdf
JZFamily <hao931126@126.com>
2019-02-17 11:19:01 +0800

    commit  finder code

--- Source/finder.py
+++ Source/finder.py
@@ -1,24 +1,58 @@
-import os 
 import hashlib
 import sqlite3
+import os
+import time
+import logging
 
-# Generate single file hash code
-def GenerateFileHash( FileName ) :
-    hs = hashlib.sha256()
-    with open(FileName,'rb') as f:
-        byte = f.read(512)
-        hs.update(byte)
-    return hs.hexdigest()
+# init log config
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(filename='my.log', level=logging.DEBUG, format=LOG_FORMAT)
 
-# 
-conn = sqlite3.connect('test.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS SRCLIB(
-        name            varchar(256) NOT NULL,
-        hash            varchar(256) NOT NULL,
-        size            numeric(18,5) NOT NULL,
-        mtine           datetime NOT NULL,
-        path            varchar(256));
-        ''')
-conn.commit()
-conn.close()
+def TimeStampToTime(timestamp):
+	timeStruct = time.localtime(timestamp)
+	return time.strftime('%Y-%m-%d %H:%M:%S',timeStruct)
+
+def GetFileHash( FileName ) :
+	hs = hashlib.sha256()
+	with open(FileName,'rb') as f:
+		byte = f.read(512)
+		hs.update(byte)
+	return hs.hexdigest()
+
+def TraversePathAndGenRecord(rootpath):
+	RootPath = os.path.abspath(rootpath)
+	logging.debug(RootPath)
+	recordset = []
+	for root, dirs, files in os.walk(RootPath) :
+		for name in files :
+			record = []
+			record.append(name)
+			FileAbsPath = root+'\\'+name
+			h = GetFileHash(FileAbsPath)
+			record.append(h)
+			record.append(os.path.getsize(FileAbsPath))
+			record.append(TimeStampToTime(os.path.getmtime(FileAbsPath)))
+			record.append(FileAbsPath)
+			logging.debug(record)
+			recordset.append(tuple(record))
+	return  recordset
+
+def GenerateDB(DBName,RootPath):
+	conn = sqlite3.connect(DBName)
+	c = conn.cursor()
+	c.execute('''create table IF NOT EXISTS srclib(
+	name varchar(256) not null,
+	hash varchar(256) not null,
+	size decimal(19,2) not null,
+	mtime datetime not null,
+	path text not null)
+	''')
+	conn.commit()
+	li = TraversePathAndGenRecord(RootPath)
+	c.executemany('''insert into srclib (name, hash, size, mtime, path) 
+	values(?,?,?,?,?)''',li)
+	conn.commit()
+	conn.close()
+
+if __name__ == "__main__" :
+	GenerateDB('test.db','.')
